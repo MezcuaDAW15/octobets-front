ce09f6e68d324fde6cce1ca326c427e4b2c7e843
MezcuaDAW15 <aw.davidmezcua@gmail.com>
2025-06-04 19:40:31 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8888/ws',
+  apiUrl: 'https://octobets.mezcu.com/api/ws',
   stripeKey: 'pk_test_51QvbeaKrtPtrsT26EuGQqkqnBrrfXgcdAUb39CxW3yOxflW3hi2Ensixme1kFvnmjeRtGUjgni4VC0AjmfVdtWf300FMISsOpb',
 
 };
